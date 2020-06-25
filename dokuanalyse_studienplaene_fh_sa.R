21dfba0205c38245d3562e81d79db010f389a3f4
Paul Soelder <p.soelder@mailbox.org>
2020-06-25 11:07:36 +0200

    init

---
+++ dokuanalyse_studienplaene_fh_sa.R
@@ -0,0 +1,17 @@
+require(knitr)
+require(markdown)
+
+# Folgende Pakete sind notwendig und werden in der rmn-Datei geladen:
+#library(pacman)
+#library(ggplot2)
+#library(reshape2)
+#library(RSQLite)
+#library(psych)
+#library(xtable)
+
+knit("dokuanalyse_studienplaene_fh_sa.rmd", "dokuanalyse_studienplaene_fh_sa.md")
+markdownToHTML("dokuanalyse_studienplaene_fh_sa.md", "dokuanalyse_studienplaene_fh_sa.html")
+
+browseURL(paste("file://",
+  file.path(getwd(), "dokuanalyse_studienplaene_fh_sa.html"),
+  sep = ""))
